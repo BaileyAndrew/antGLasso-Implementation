db8c933b0a9fa97bf2500d1a4cd2990a89018f6d
Bailey Andrew <e.bailey.andrew@gmail.com>
2022-06-29 12:42:59 +0100

    Heuristic now works for Tensor data

--- Python Implementation/Scripts/nonparanormal_skeptic.py
+++ Python Implementation/Scripts/nonparanormal_skeptic.py
@@ -22,15 +22,15 @@ def nonparanormal_tensor_skeptic(
     Ys: "(m, *ds)"
 ):
     m, *ds = Ys.shape
-    Ss = [np.empty((m, p:=np.prod(ds)//d, p)) for d in ds]
+    Ss = [np.empty((m, d, d)) for d in ds]
     
     for idx, d in enumerate(ds):
         dim = idx+1
-        Ys_dim = np.moveaxis(Ys, dim, -1)
+        Ys_dim = np.moveaxis(Ys, dim, 1)
         for batch in range(m):
             Ss[idx][batch] = 2 * np.sin(np.pi / 6 * spearmanr(
-                Ys_dim[batch].reshape(-1, d), axis=1
+                Ys_dim[batch].reshape(-1, np.prod(ds)//d), axis=1
             )[0])
         Ss[idx] = Ss[idx].mean(axis=0)
         np.fill_diagonal(Ss[idx], 1)
-    return Ss
+    return Ss[::-1] #b/c this func made them in reversed order
